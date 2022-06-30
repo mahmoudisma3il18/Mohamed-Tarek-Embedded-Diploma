b8d03c311fd63a9dacfd45f0ed750b65c482328b
mahmoudisma3il18 <mody.bg12@gmail.com>
2022-06-30 02:30:17 +0200

    .

--- Assigment 1/Qes-6/main.c
+++ Assigment 1/Qes-6/main.c
@@ -2,6 +2,19 @@
 
 int main()
 {
-    printf("Hello World!\n");
-    return 0;
+    int num1,num2;
+    printf("Enter number1 : ");
+    scanf("%d",&num1);
+    printf("Enter number2 : ");
+    scanf("%d",&num2);
+
+    if(num1 == num2 )
+        printf("Both numbers are eqaul\n");
+
+     else {
+            if(num1 > num2)
+                printf("%d is greater than %d\n",num1,num2);
+            else
+                printf("%d is greater than %d\n",num2,num1);
+        }
 }
