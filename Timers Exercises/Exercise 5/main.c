b4caf27f6fffce7c7e21b2e87d55b14012c3a3a0
mahmoudisma3il18 <mody.bg12@gmail.com>
2022-10-10 14:17:29 +0200

    TIMER EXERCISES

---
+++ Timers Exercises/Exercise 5/main.c
@@ -0,0 +1,33 @@
+/*
+ * Exercise 5.c
+ *
+ * Created: 10/10/2022 1:40:50 PM
+ * Author : modyb
+ */ 
+
+#define  F_CPU 1000000
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+void Timer0_PWM_Init(unsigned char ticks){
+	DDRB |= (1<<PB3);
+	TCNT0 = 0x00;
+	OCR0 = ticks;
+	TCCR0 = (1<<WGM01) | (1<<WGM00)  | (1<<COM01) | (1<<CS01);
+	
+}
+
+int main(void)
+{
+    /* Replace with your application code */
+	
+	Timer0_PWM_Init(128);
+	_delay_ms(5000);
+	Timer0_PWM_Init(64);
+    while (1) 
+    {
+    }
+}
+
---
+++ Timers Exercises/Exercise 6/main.c
@@ -0,0 +1,18 @@
+/*
+ * Exercise 6.c
+ *
+ * Created: 10/10/2022 1:53:30 PM
+ * Author : modyb
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
