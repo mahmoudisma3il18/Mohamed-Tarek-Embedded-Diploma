850c189d16fbd731125103897f0321e10c48b4a7
Mahmoud Ismail <mody.bg12@gmail.com>
2023-08-04 16:38:25 +0300

    changed some files locations

--- Atemga16 Drivers/lm35_sensor/Debug/lm35_sensor.d
+++
@@ -1,6 +0,0 @@
-lm35_sensor.d lm35_sensor.o: .././lm35_sensor.c .././lm35_sensor.h \
- d:\embedded\ systems\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint-gcc.h
-
-.././lm35_sensor.h:
-
-d:\embedded\ systems\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint-gcc.h:
